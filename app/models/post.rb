4f594d46c28282217436ce414f7425702618e4b2
psicomago <carlosaravena.coach@gmail.com>
2022-06-28 23:58:00 -0400

    add some bootstraps

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
     validates :image_url ,presence: :true
     validates :title ,presence: :true
     validates :content ,presence: :true
+
+    before_save :update_content
+    def update_content
+        ephemeral = content.downcase.gsub("spoiler", "")
+        self.content = ephemeral
+    end
 end
